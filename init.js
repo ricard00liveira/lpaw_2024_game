1be01b9c385f44b76202ab2948b725ebaa0aec29
Ricardo P Oliveira <ricardooliveira.pl216@academico.ifsul.edu.br>
2024-09-17 22:53:46 -0300

    fixed some bugs

--- init.js
+++ init.js
@@ -60,7 +60,7 @@ const init = async () => {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    console.log("x: " + x + ", y: " + y);
+    //console.log("x: " + x + ", y: " + y);
     // Verificar se o clique está dentro do botão
     if (
       x > buttonX &&
